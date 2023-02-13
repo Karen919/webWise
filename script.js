feb17fe903729b6b017eba39702a72c9442163d4
Karen919 <ka.arakelyan@mail.ru>
2023-02-14 01:21:00 +0400

    fdfd

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-document.getElementById('js').addEventListener('click', JsNone);
-function JsNone() {
- document.getElementById('jsNone').classList.toggle('jsNone');
+//****************************************** */ homeButtonsCall
+document.getElementById('js').addEventListener('click',function () { none ('jsNone')});
+document.getElementById('window').addEventListener('click',function () { none ('windowNone')});
+document.getElementById('dom').addEventListener('click',function () { none ('domNone')});
+document.getElementById('events').addEventListener('click', function () { none ('eventNone')});
+document.getElementById('api').addEventListener('click', function(){ none('apiNone')  });
+function none(x) {
+ document.getElementById(x).classList.toggle(x);
 }
-document.getElementById('window').addEventListener('click', windowNone);
-function windowNone() {
- document.getElementById('windowNone').classList.toggle('windowNone');
-}
-document.getElementById('dom').addEventListener('click', domNone);
-function domNone() {
- document.getElementById('domNone').classList.toggle('domNone');
-}
-document.getElementById('events').addEventListener('click', eventNone);
-function eventNone() {
- document.getElementById('eventNone').classList.toggle('eventNone');
-}
-document.getElementById('api').addEventListener('click', apiNone);
-function apiNone() {
- document.getElementById('apiNone').classList.toggle('apiNone');
-}
+
+
+let sectionHtmlDocument = document.getElementById('sectionHtmlDocument');
+let htmlDocument = 'sectionHtmlDocument';
+document.getElementById('htmlDocuments').addEventListener('click', function() { changemain(sectionHtmlDocument, htmlDocument); });
+
+function changemain ( x, y) {
+ 
+   document.getElementById('main').innerHTML = x.innerHTML;
+   document.getElementById(y).classList.toggle(y);
+};
+// ******************************************************htmlDomMethodsCall
+
+function htmlDomMethodsCall (x) {
+ document.getElementById('htmlDocumentHeader').style.display = 'none';
+ document.getElementById(x).classList.toggle('none');
+ 
+};
+
+
+
+
 
 //******************************************************* */ array
 
@@ -25,27 +36,4 @@ document.getElementById('array').addEventListener('click', array);
 function array () {
   document.getElementById('section').style.display= 'none';
   document.getElementById('sectionArray').style.display = 'initial';
-}
-
-// **************************html documents
-let htmlDocumentButton = document.getElementById('htmlDocumentsButton').addEventListener('click', htmlDocumentBUtton);
-function htmlDocumentBUtton () {
-    document.getElementById('htmlDocumentHeader').style.display = 'initial';
-    document.getElementById('htmlDocumentBoddy').style.display = 'none';
-}
-
-document.getElementById('htmlDocuments').addEventListener('click', htmlDocumnets);
-
-function htmlDocumnets () {
-  document.getElementById('section').style.display = 'none';
-  document.getElementById('sectionHtmlDocument').style.display = 'initial';
-}
-
-//8*************************8 html document***activeElement
-document.getElementById('activeElementButton').addEventListener('click', activeElementFunction);
-function activeElementFunction() {
-  document.getElementById('htmlDocumentHeader').style.display = 'none';
-  document.getElementById('htmlDocumentBoddy').style.display = 'initial';
-
-   document.getElementById('activeElement').style.display = 'initial';
 }
